f31a11afd49591dd2d25d886d4633179782994da
ullas121 <ullasach@gmail.com>
2020-03-29 21:42:49 +0530

    m2 is created

--- TestProject/src/com/testing/A.java
+++ TestProject/src/com/testing/A.java
@@ -5,6 +5,17 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("printing from A class main method ");
 
+		A a = new A();
+		a.m1();
 	}
+	public void m1()
+	{
+		System.out.println("this is the method 1");
+	}
+	
+	public static void m2() {
+		System.out.println("this is the method 2 ");
+	}
+	
 
 }
